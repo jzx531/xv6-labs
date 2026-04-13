11bb1813a4178b0054ed03feb73366f881e0077f
jzx531 <115254510+jzx531@users.noreply.github.com>
2026-04-13 23:35:35 +0800

    Update sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -1,3 +1,9 @@
+
+/*
+请参阅kernel/sysproc.c以获取实现sleep系统调用的xv6内核代码（查找sys_sleep），
+user/user.h提供了sleep的声明以便其他程序调用，
+用汇编程序编写的user/usys.S可以帮助sleep从用户区跳转到内核区。
+*/
 #include "kernel/types.h"
 // #include "kernel/stat.h"
 #include "user/user.h"
